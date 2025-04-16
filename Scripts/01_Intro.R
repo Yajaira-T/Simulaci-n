fafbe80fa6c015b49c255a349cff9f7e8ddf1b35
Yajaira-T <yajaira.toaquiza@epn.edu.ec>
2025-04-16 15:42:54 +0000

    comentario

--- Scripts/01_Intro.R
+++ Scripts/01_Intro.R
@@ -1,6 +1,24 @@
 #Primer script del curso
+#Configuraciones iniciales
+179/5378641
+options(scipen = 999) #eliminar notacion cientifica 
+options (digits = 2) # que salga con 4 digitos 
+
+#Instalacion de paquetes
+install.packages("data.table")
+install.packages("tidyverse", dependencies =TRUE)
+library(data.table)
+library(tidyverse)
+
+#Rprofile
+if(!file.exists("~/.Rprofile")){
+  file.create("~/.Rprofile")
+} else {
+  file.edit("~/.Rprofile")
+}
 
 #Vectores
+
 c(2,5,8,12)
 sum(c(2,5,8,12))
 mean(c(2,5,8,12))
@@ -8,3 +26,5 @@ mean(c(2,5,8,12))
 x <-c(2,5,8,12)
 sum(x)
 mean(x)
+
+

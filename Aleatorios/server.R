f6dcc3dae9d763a069f20c3ed1a0ff6b27d41c2a
Yajaira-T <yajaira.toaquiza@epn.edu.ec>
2025-05-14 21:37:20 -0500

    cambios A

--- Aleatorios/server.R
+++ Aleatorios/server.R
@@ -1,29 +1,98 @@
-
 library(shiny)
 library(kableExtra)
-#función que genra numeros aleatorios bajo el método congruencial
-random_cong <-function(a, m, x0, n){
-  res<-numeric(n+1)
-  res[1]<-x0
+library(data.table)
+
+# Funciòn que genera numeros aleatorios bajo el método congruencial mutiplicativo
+random_cong <- function(a, m, x0, n){
+  res <- numeric(n+1)
+  res[1] <- x0
   for(k in 2:length(res)){
-    res[k]<-(a*res[k-1] )%% m
+    res[k] <- (a*res[k-1]) %% m
   }
-  return(res[-1]) # para que no me devuelva la semilla
+  return(res[-1]/m)
 }
-random_cong(a=5, x0=3, m=7, n=6)
+
+# Función que genera numeros aleatorios bajo el método congruencial mixto
+random_mixt <- function(a, c, m, x0, n){
+  res <- numeric(n+1)
+  res[1] <- x0
+  for(k in 2:length(res)){
+    res[k] <- (a*res[k-1] + c) %% m
+  }
+  return(round((res[-1])/m,6))
+}
+
+conv_matrix <- function(vector, cols=10){
+  res <- rep(NA_real_, ceiling(length(vector)/cols)*cols) #ceiling redondea hacia arriba
+  res[1:length(vector)] <- vector
+  res <- as.data.frame(matrix(res, nrow = ceiling(length(vector)/cols), ncol = cols, byrow = TRUE))
+  colnames(res) <- paste0("Cols", 1:cols) #paste0() concatena texto sin espacios.
+  return(res)
+}
+
+conv_matrix(c(1,2,3,4,5,6,7,8), cols = 5)
+
 
 # Define server logic required to draw a histogram
 function(input, output, session) {
+  
+  coords <- eventReactive(input$calcular, {
+    x_vals <- seq(input$lim_inf, input$lim_sup, length.out = 100)
+    y_vals <- sapply(x_vals, function(x){eval(parse(text = input$funcion))})
+    coords <- data.frame(x = x_vals, y = y_vals)
+  })
+  
   output$tabla <- function(){
     
-    res <<-data.table(Valor=random_cong(a=input$constante, m=input$divisor, x0= input$semilla, n=input$num)/input$divisor)
-    res[,n:=1:nrow(res)]
-    res<-res[, c("n", "Valor"), with= FALSE]
+    res <- conv_matrix(random_cong(a = input$constante, m = input$divisor, x0 = input$semilla, n = input$num))
     
-    kbl(res, booktabs = TRUE, escape=FALSE) %>% 
+    kbl(res, booktabs = TRUE, escape = FALSE) %>% 
       kable_styling(full_width = FALSE, bootstrap_options = c("bordered"), font_size = 12) %>%
-      row_spec(0, background = "#1D3889", color = "#ffffff")
-    
+      row_spec(0, background = "#1D3889", color = "#ffffff") %>% scroll_box(width = "100%", height = "200px")
+    #scroll_box(...)Hace que la tabla sea desplazable si ocupa más de 200px 
   }
-
+  
+  output$graf_fun01 <- renderPlot({
+    f <- function(x) eval(parse(text = input$funcion))
+    datos <- coords()
+    delta_x <- (input$lim_sup - input$lim_inf) / (length(datos$x) - 1)
+    area <- (delta_x / 2) * (f(input$lim_inf) + 2 * sum(f(datos$x[2:(length(datos$x) - 1)])) + f(input$lim_sup))
+    
+    datos %>% ggplot(aes(x = x, y = y)) + geom_line(color = "blue", linewidth = 1) +
+      geom_area(mapping = aes(x = x, y = y), fill = "lightblue", alpha = 0.3) +
+      geom_vline(xintercept = input$lim_inf, linetype = "dashed", color = "red") +
+      geom_vline(xintercept = input$lim_sup, linetype = "dashed", color = "red") +
+      labs(title = paste("Área bajo la curva de f(x) =", input$funcion),
+           subtitle = paste("Intervalo:", input$lim_inf, "a", input$lim_sup),
+           x = "x",
+           y = "f(x)",
+           caption = paste("Área aproximada:", round(area, 4))
+      ) + theme_minimal()
+  })
+  
+  output$graf_aprox01 <- renderPlot({
+    secuencia <- seq(100, 10000, by = 250)
+    f <- function(x) eval(parse(text = input$funcion))
+    
+    if(input$lim_inf == 0 & input$lim_sup == 1){
+      data.table(Aproximacion = sapply(secuencia, function(k){
+        res <- random_cong(a = 7^5, m = 2^31 - 1, x0 = as.numeric(now()), n = k)
+        mean(sapply(res, function(x){eval(parse(text = input$funcion))}))
+      }), 
+      Teorico = integrate(f, lower=0, upper = 1)$value
+      ) %>% gather(key = "Etiqueta", value = "Valor") %>% mutate(Aleatorios = c(secuencia, secuencia)) %>% 
+        ggplot(aes(x=Aleatorios, y = Valor, group = Etiqueta, , colour = Etiqueta)) + geom_line()
+    } else if(input$lim_inf !=0 | input$lim_sup !=1){
+      data.table(Aproximacion = sapply(secuencia, function(k){
+        res <- input$lim_inf + (input$lim_sup - input$lim_inf)*random_cong(a = 7^5, m = 2^31 - 1, x0 = as.numeric(now()), n = k)
+        mean(sapply(res, function(x){(input$lim_sup - input$lim_inf)*eval(parse(text = input$funcion))}))
+      }), 
+      Teorico = integrate(f, lower=input$lim_inf, upper = input$lim_sup)$value
+      ) %>% gather(key = "Etiqueta", value = "Valor") %>% mutate(Aleatorios = c(secuencia, secuencia)) %>% 
+        ggplot(aes(x=Aleatorios, y = Valor, group = Etiqueta, , colour = Etiqueta)) + geom_line()
+    }
+    
+    
+  })
+  
 }
--- Aleatorios/ui.R
+++ Aleatorios/ui.R
@@ -1,37 +1,85 @@
 
-
 library(shiny)
 library(kableExtra)
+
 # Define UI for application that draws a histogram
 fluidPage(
-
-    # Application title
-    titlePanel("Generación de números aleatorios - Método congruencial"),
-
-    # Sidebar with a slider input for number of bins
-    sidebarLayout(
-        sidebarPanel(
-            numericInput("semilla",
-                        "Ingrese un valor inicial:",
-                        min=0,
-                        value = 30),
-            numericInput("divisor",
-                        "Ingrese un valor de m:",
-                        min=0,
-                        value = 30),
-            numericInput("constante",
-                        "Ingrese un valor de a:",
-                        min=0,
-                        value = 30),
-            numericInput("num",
-                        "Cantidad de números a generar:",
-                        min=0,
-                        value = 30),
-        ),
-# 
-        # Show a plot of the generated distribution
-        mainPanel(
-            tableOutput("tabla")
-        )
+  # Application title
+  titlePanel("Generación de números aleatorios - Método congruencial"),
+  
+  tabsetPanel(
+    tabPanel("Nùmeros Aleatorios",
+             # Sidebar with a slider input for number of bins
+             sidebarLayout(
+               sidebarPanel(
+                 sliderInput("semilla",
+                             "Ingrese un valor inicial:",
+                             min = 1,
+                             max = 500,
+                             value = 30),
+                 sliderInput("divisor",
+                             "Ingrese el valor de m:",
+                             min = 1,
+                             max = 500,
+                             value = 37),
+                 sliderInput("constante",
+                             "Ingrese el valor de a:",
+                             min = 1,
+                             max = 500,
+                             value = 123),
+                 sliderInput("num",
+                             "Cantidad de números a generar:",
+                             min = 1,
+                             max = 200,
+                             value = 30),
+                 actionButton("mostrar", "Mostrar resultados", icon("eye"))
+               ),
+               
+               # Show a plot of the generated distribution
+               mainPanel(
+                 conditionalPanel(condition = "input.mostrar!=0",
+                                  h4("Tabla de resultados - Método Congruencial Multiplicativo:"),
+                                  br(), #genera una linea en blanco
+                                  tableOutput("tabla"),
+                                  br(),
+                                  h4("Tabla de resultados - Método Congruencial Mixto:"),
+                                  br(),
+                                  br(), # Aqui va la segunda tabla
+                                  br(),
+                                  h4("Gráfico:"),
+                                  fluidRow(
+                                    column(width = 2,
+                                           numericInput("barras", "Número de barras:", value = 10, min = 2, max = 20),
+                                    ),
+                                    column(width = 5,
+                                           h5("Método 1")
+                                    ),
+                                    column(width = 5,
+                                           h5("Método 2")
+                                    )
+                                  ))
+               )
+             )
+    ),
+    tabPanel("Integrales",
+             sidebarLayout(
+               sidebarPanel(
+                 textInput("funcion", "Ingrese la funciòn a integrar:", value = "1-x"),
+                 numericInput("lim_inf", "Límite inferior del intervalo:", value = 0),
+                 numericInput("lim_sup", "Límite superior del intervalo:", value = 1),
+                 radioButtons("metodo", "Seleccione el mètodo para generar los nùmeros aleatorios:", 
+                              c("Congruencial Multiplicativo", "Congruencial Mixto")),
+                 actionButton("calcular", "Calcular Àrea", style = "color: #FFFFFF; background-color: #F5426C; border-color: #9932CC")
+               ),
+               mainPanel(
+                 conditionalPanel(condition = "input.calcular!=0",
+                                  h4("Gráfica de la funcion a integrar:"),
+                                  plotOutput("graf_fun01"),
+                                  h4("Aproximaciòn:"),
+                                  plotOutput("graf_aprox01")
+                 )
+               )
+             )
     )
+  )
 )
